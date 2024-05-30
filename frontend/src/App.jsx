0e3244111c78626d1c38245357a88d40059bfc88
madhavmadupu <134821170+madhavmadupu@users.noreply.github.com>
2024-05-30 22:14:07 +0530

    added Tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <div className='border px-10 py-5 rounded-xl font-black'>
+      Hello
+      </div>
+    </div>
+  );
+}
+
+export default App;
