8533d1c5f92a040790f78c04d36a758d46f87948
madhavmadupu <134821170+madhavmadupu@users.noreply.github.com>
2024-05-31 13:55:09 +0530

    Update Form.jsx

--- frontend/src/TaskBoard/Form.jsx
+++ frontend/src/TaskBoard/Form.jsx
@@ -8,39 +8,70 @@ const Form = () => {
         id="firstName"
         name="firstName"
         placeholder='Title'
-        className='border rounded-[20px] w-full border-[#1E1E1E] text-[#1E1E1E] px-10 py-2 text-4xl'
+        className='border rounded-[20px] w-full border-[#1E1E1E] text-[#1E1E1E] px-8 py-2 text-4xl'
         />
         <textarea
         type="text"
         id="firstName"
         name="firstName"
         placeholder='Description'
-        className='border rounded-[20px] w-full border-[#1E1E1E] text-[#1E1E1E] px-10 py-2'
+        className='border rounded-[20px] w-full border-[#1E1E1E] text-[#1E1E1E] p-8'
         rows={14}
         />
 
-        <div className='flex flex-row justify-between gap-4'>
-            <select
-                id="category"
-                name="category"
-                className='border rounded-[20px] w-full border-[#1E1E1E] text-[#1E1E1E] py-5 flex justify-center items-center'
-            >
-                <option value="select tag">select tag</option>
-                <option value="personal">Personal</option>
-                <option value="work">Work</option>
-                <option value="home">Home</option>
-            </select>
+        <div className='flex flex-col justify-between gap-4'>
+            <div className='flex flex-row justify-between items-center w-full gap-[21px]'>
+                <select
+                    id="category"
+                    name="category"
+                    className='border rounded-[20px] w-full border-[#1E1E1E] text-[#1E1E1E] py-4 flex justify-center items-center bg-transparent text-center font-black'
+                >
+                    <option value="select tag">select tag</option>
+                    <option value="personal">Personal</option>
+                    <option value="work">Work</option>
+                    <option value="home">Home</option>
+                </select>
 
-            <select
-                id="category"
-                name="category"
-                className='border rounded-[20px] w-full border-[#1E1E1E] text-[#1E1E1E] px-10 py-2 flex justify-center items-center'
-            >
-                <option value="select tag">select tag</option>
-                <option value="personal">Personal</option>
-                <option value="work">Work</option>
-                <option value="home">Home</option>
-            </select>
+                <select
+                    id="category"
+                    name="category"
+                    className='border rounded-[20px] w-full border-[#1E1E1E] text-[#1E1E1E] py-4 flex justify-center items-center bg-transparent text-center font-black'
+                >
+                    <option value="select tag">date & time</option>
+                    <option value="personal">Personal</option>
+                    <option value="work">Work</option>
+                    <option value="home">Home</option>
+                </select>
+            </div>
+
+            <div className='flex flex-row justify-between items-center w-full gap-[21px]'>
+                <select
+                        id="category"
+                        name="category"
+                        className='border rounded-[20px] w-full border-[#1E1E1E] text-[#1E1E1E] py-4 flex justify-center items-center bg-transparent text-center font-black'
+                    >
+                    <option value="select tag">select priority</option>
+                    <option value="personal">Now</option>
+                    <option value="work">Soon</option>
+                    <option value="home">Later</option>
+                </select>
+
+                <select
+                    id="category"
+                    name="category"
+                    className='border rounded-[20px] w-full border-[#1E1E1E] text-[#1E1E1E] py-4 flex justify-center items-center bg-transparent text-center font-black'
+                >
+                    <option value="select tag">repeat frequency</option>
+                    <option value="personal">Daily</option>
+                    <option value="work">Weekly</option>
+                    <option value="home">Monthly</option>
+                    <option value="home">Yearly</option>
+                </select>
+            </div>
+
+            <button className='py-4 border rounded-[20px] w-full bg-[#F04D23] font-black text-white'>SAVE CHANGES</button>
+            <button className='py-4 border rounded-[20px] w-full border-[#F04D23] font-black '>DISCARD</button>
+            
         </div>
         
     </div>
