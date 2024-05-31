537b75ac7dadd50693168da991e850e77ca23394
Tarun Vuppala <tarun.vuppala26@gmail.com>
2024-06-01 01:33:42 +0530

    login and signup

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 function SignUp() {
     return (
         <>
-            <h1>Signup</h1>
             <div className="flex items-center justify-center bg-transparent">
                 <div className="min-h-screen-flex justify-center bg-white p-8 rounded-tl-2xl shadow-lg w-[500px] h-[500px] ">
                     <div className='flex md:flex-col'>
@@ -14,22 +13,24 @@ function SignUp() {
                             </button>
 
                             <button className={" border border-[#F04D23] mb-4 font-bold text-black py-2 px-12  rounded-[20px] "}>
-                                LOGIN
+
+                                <Link to="/login">LOGIN</Link>
+
                             </button>
                         </div>
                     </div>
                     <h1 className="flex font-bold items-center justify-center text-4xl">SIGN UP</h1>
                     <form action="signup" className="flex-col" >
-                        <input type="text" name="username" id="" placeholder="username" className="border mt-5 mb-4 size-full border-black rounded-[20px] py-2 px-9 "/>
-                        <input type="text" name="username" id="" placeholder="email" className="border mb-4 size-full border-black rounded-[20px] py-2 px-9"/>
-                        <input type="text" name="username" id="" placeholder="password" className="border size-full mb-4 border-black rounded-[20px] py-2 px-9"/>
-                        <input type="text" name="username" id="" placeholder="confirm password" className="border size-full mb-4 border-black rounded-[20px] py-2 px-9"/>
-                        <input type="text" name="username" id="" placeholder="mobile number" className="border size-full mb-4 border-black rounded-[20px] py-2 px-9"/>
+                        <input type="text" name="username" id="" placeholder="username" className="border mt-5 mb-4 size-full border-black rounded-[20px] py-2 px-9 " />
+                        <input type="email" name="email" id="" placeholder="email" className="border mb-4 size-full border-black rounded-[20px] py-2 px-9" />
+                        <input type="password" name="password" id="" placeholder="password" className="border size-full mb-4 border-black rounded-[20px] py-2 px-9" />
+                        <input type="password" name="confirm_password" id="" placeholder="confirm password" className="border size-full mb-4 border-black rounded-[20px] py-2 px-9" />
+                        <input type="text" name="phno" id="" placeholder="mobile number" className="border size-full mb-4 border-black rounded-[20px] py-2 px-9" />
+                        <button className={"flex  justify-center bg-[#F04D23] text-white font-bold mb-4 rounded-[20px] py-2 px-9"}>
+                            SIGN UP
+                        </button>
                     </form>
 
-                    <button className={"flex  justify-center bg-[#F04D23] text-white font-bold mb-4 rounded-[20px] py-2 px-9"}>
-                                SIGN UP
-                    </button>
 
                 </div>
             </div>
@@ -37,7 +38,6 @@ function SignUp() {
 
 
 
-            <Link to="/login">Login</Link>
         </>
     )
 }
