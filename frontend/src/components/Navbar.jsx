ac90ca3549c241670c9d7305f41568846a09a0fd
Tarun Vuppala <tarun.vuppala26@gmail.com>
2024-05-31 15:31:05 +0530

    handleClick Form

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,15 +3,17 @@ import React, { useState } from 'react'
 
 const Navbar = () => {
   const [activeTab, setActiveTab] = useState("Task Board");
-  
 
+  const handleClick = () => {
+    console.log("clicked")
+  }
 
   return (
     <div className='w-full flex md:flex-col'>
       <div className='flex flex-row items-center w-[50%] justify-center gap-[21px]'>
-        <button className='bg-[#F04D23] p-4  font-black text-white rounded-[20px]' onClick={alert("New Note Created")} >
+        <button className='bg-[#F04D23] p-4  font-black text-white rounded-[20px]' onClick={handleClick}>
           <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M16.136 9.864H10.056V16.344H6.472V9.864H0.424V6.648H6.472V0.184H10.056V6.648H16.136V9.864Z" fill="white"/>
+            <path d="M16.136 9.864H10.056V16.344H6.472V9.864H0.424V6.648H6.472V0.184H10.056V6.648H16.136V9.864Z" fill="white" />
           </svg>
         </button>
         <button className={`${activeTab === "Task Board" ? "bg-[#F04D23] text-white" : "border border-[#F04D23] text-black"} py-4 font-black  rounded-[20px] px-[50px]`} onClick={() => setActiveTab("Task Board")}>
