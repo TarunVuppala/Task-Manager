5150eb3747939cddc2ea215db42492037e37f9f2
madhavmadupu <134821170+madhavmadupu@users.noreply.github.com>
2024-05-31 15:18:16 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,9 +7,9 @@ const Navbar = () => {
 
 
   return (
-    <div className='w-full dark'>
+    <div className='w-full flex md:flex-col'>
       <div className='flex flex-row items-center w-[50%] justify-center gap-[21px]'>
-        <button className='bg-[#F04D23] p-4  font-black text-white rounded-[20px]'>
+        <button className='bg-[#F04D23] p-4  font-black text-white rounded-[20px]' onClick={alert("New Note Created")} >
           <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M16.136 9.864H10.056V16.344H6.472V9.864H0.424V6.648H6.472V0.184H10.056V6.648H16.136V9.864Z" fill="white"/>
           </svg>
