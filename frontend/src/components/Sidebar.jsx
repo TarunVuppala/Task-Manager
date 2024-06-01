2b6a2fc28d8a202a35e228e67c2ca0358ee5e80e
madhavmadupu <134821170+madhavmadupu@users.noreply.github.com>
2024-06-02 00:28:28 +0530

    changes

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
 
 function Sidebar() {
     return (
         <div className='h-screen w-[500px] border-r'>
             <div className='flex flex-col justify-between items-center'>
-                <div className="border my-8">Lefrtwgefnrehtgsfwdhntyergefhntyrgseft Top</div>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DateCalendar />
+                </LocalizationProvider>
+
                 <div className='flex w-full h-[1px] bg-black'></div>
-                <div className="border my-8">Lefrtwgefnrehtgsfwdhntyergefhntyrgseft Top</div>
                 
                 <button className="bg-[#F04D23] text-white py-2 px-12 font-bold rounded-[20px]">
                     LOGOUT
