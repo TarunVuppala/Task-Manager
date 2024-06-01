1e8419d38cbe28efce65471416acd7fd39c6afb2
bollojuvinay <bollojulucky11@gmail.com>
2024-06-01 10:09:45 +0530

    merge

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,20 +5,29 @@ import './App.css';
 import Home from './Home';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
-// import NoteForm from './Notes/NoteForm';
+import NoteForm from './Notes/NoteForm';
+
 
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path='/' element={<Home/>} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/signup' element={<SignUp />} />
-          {/* <Route path='/note' element={<NoteForm/>}/> */}
+      <div className='flex flex-row justify-between items-center h-screen'>
+        <div className='w-[50%] flex flex-col justify-between items-center'>
+          <div>Left Top</div>
+          <div className='w-[90%] h-[1px] bg-black'></div>
+          <div>Left Bottom</div>
+        </div>
 
-        </Routes>
-      </Router>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/signup' element={<SignUp />} />
+          <Route path='/note' element={<NoteForm/>}/>
+          </Routes>
+        </Router>
+      </div>
+
     </>
 
   );
