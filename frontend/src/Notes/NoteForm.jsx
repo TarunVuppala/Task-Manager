d6b2bddc112172f08274f039d3c572c2e23e7529
bollojuvinay <bollojulucky11@gmail.com>
2024-06-01 22:03:19 +0530

    noteform buttons and spacing

--- frontend/src/Notes/NoteForm.jsx
+++ frontend/src/Notes/NoteForm.jsx
@@ -3,20 +3,20 @@ import React from 'react'
 function NoteForm() {
     return (
         <>
-        
+            <div className='flex flex-row'></div>
             <input
                 type="text"
                 id="firstName"
                 name="firstName"
                 placeholder='Title'
-                className='border rounded-[20px] w-full border-[#1E1E1E] text-[#1E1E1E] px-8 py-2 text-4xl'
+                className='border rounded-[20px] m-4 mt-8 w-full border-[#1E1E1E] text-[#1E1E1E] px-8 py-2 text-4xl'
             />
             <textarea
                 type="text"
                 id="firstName"
                 name="firstName"
                 placeholder='Description'
-                className='border rounded-[20px] w-full border-[#1E1E1E] text-[#1E1E1E] p-8'
+                className='flex border rounded-[20px] ml-4 w-full border-[#1E1E1E] text-[#1E1E1E] p-8'
                 rows={14}
             />
 
@@ -26,8 +26,8 @@ function NoteForm() {
                 </div>
 
                 <div className='flex flex-row justify-start items-center'>
-                    <button className='py-4 px-10 border rounded-[20px]  bg-[#F04D23] font-black text-white'>SAVE CHANGES</button>
-                    <button className='py-4 px-7 ml-4 border rounded-[20px]  border-[#F04D23] font-black '>DISCARD</button>
+                    <button className='py-3 px-5 ml-4 border rounded-[20px]  bg-[#F04D23] font-black text-white'>Add Note</button>
+                    <button className='py-3 px-5 ml-4 border rounded-[20px]  border-[#F04D23] font-black '>Discard</button>
                 </div>
                 
             </div>
