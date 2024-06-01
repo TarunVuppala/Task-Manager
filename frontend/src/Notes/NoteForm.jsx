bc32a7c78e9e895cc91358c457a3c037ddcb6e6e
bollojuvinay <bollojulucky11@gmail.com>
2024-06-01 16:05:28 +0530

    notes form and notes page

--- frontend/src/Notes/NoteForm.jsx
+++ frontend/src/Notes/NoteForm.jsx
@@ -3,12 +3,35 @@ import React from 'react'
 function NoteForm() {
     return (
         <>
-        <h1>Note Form</h1>
-            
+        
+            <input
+                type="text"
+                id="firstName"
+                name="firstName"
+                placeholder='Title'
+                className='border rounded-[20px] w-full border-[#1E1E1E] text-[#1E1E1E] px-8 py-2 text-4xl'
+            />
+            <textarea
+                type="text"
+                id="firstName"
+                name="firstName"
+                placeholder='Description'
+                className='border rounded-[20px] w-full border-[#1E1E1E] text-[#1E1E1E] p-8'
+                rows={14}
+            />
+
+            <div className='flex flex-col justify-between gap-4'>
+                <div className='flex flex-row justify-between items-center w-full gap-[21px]'>
+                    
+                </div>
+
+                <div className='flex flex-row justify-start items-center'>
+                    <button className='py-4 px-10 border rounded-[20px]  bg-[#F04D23] font-black text-white'>SAVE CHANGES</button>
+                    <button className='py-4 px-7 ml-4 border rounded-[20px]  border-[#F04D23] font-black '>DISCARD</button>
+                </div>
+                
+            </div>
         </>
-
-
-
     );
 }
 export default NoteForm;
--- frontend/src/Notes/NotesPage.jsx
+++ frontend/src/Notes/NotesPage.jsx
@@ -1,12 +1,56 @@
-import React from "react";
+import React , {useState} from "react";
 function NotesPage() {
+    
     return (
         <div className='flex flex-row'>
             <div className='flex flex-col'>
-                <h1 className='font-black text-5xl'>NOTES</h1>
+                <div className='flex flex-row items-center justify-between'>
+                
+                <h1 className='font-black text-5xl m-8 px-50'>
+                    Notess
+                </h1>
+                    <select
+                        id="category"
+                        name="category"
+                        className='border rounded-[20px]  text-[#1E1E1E] py-1 flex  bg-transparent  font-black'
+                    >
+                        <option value="select tag">|||</option>
+                        <option value="personal">Personal</option>
+                        <option value="work">Work</option>
+                        <option value="home">Home</option>
+                    </select>
+                </div>
+
+                <div>
+                    <div className="p-4 grid grid-cols gap-4 sm:grid-cols-2 lg:grid-cols-3 ">
+                        {Array.from({ length: 6 }).map((_, index) => (
+                            <div key={index} className="shadow-lg p-4 rounded-lg">
+                                <label htmlFor={`input${index}`} className="block text-xl font-bold text-gray-700 mb-3">
+                                    Note {index + 1}
+                                </label>
+                                <p className=''>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos expedita provident amet modi deleniti facilis reprehenderit repellat tempore? Dignissimos facere aliquam cumque nobis incidunt porro.</p>
+                                <div className=" flex flex-row items-end justify-end pt-10">
+                                    <button><i class="fa-solid fa-trash"></i></button>
+                                </div>
+
+                            </div>
+                        ))}
+                    </div>
+
+
+
+
+
+
+
+
+                </div>
             </div>
         </div>
     );
+    
 }
+
+
 
 export default NotesPage;
