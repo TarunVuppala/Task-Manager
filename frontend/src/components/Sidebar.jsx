cc557d8a17e7bb84a2e74fe9e08c66b81da7ef24
madhavmadupu <134821170+madhavmadupu@users.noreply.github.com>
2024-06-01 15:58:51 +0530

    changes

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -2,14 +2,16 @@ import React from "react";
 
 function Sidebar() {
     return (
-        <div className='flex flex-row justify-between items-center h-screen w-[40%]'>
+        <div className='flex flex-row justify-between items-center w-[40%] shadow-xl rounded-[20px] bg-[#e1e1e1] h-[90%]'>
             <div className='flex flex-col justify-between items-center p-6'>
                 <div className="border my-8">Lefrtwgefnrehtgsfwdhntyergefhntyrgseft Top</div>
                 <div className='flex w-full h-[1px] bg-black'></div>
                 <div className="border my-8">Lefrtwgefnrehtgsfwdhntyergefhntyrgseft Top</div>
                 
+                <button className="bg-[#F04D23] text-white mb-4 text-black py-2 px-12 font-bold rounded-[20px]">
+                    LOGOUT
+                </button>
             </div>
-            <div className='w-[1px] h-screen bg-black'></div>
         </div>
     )
 };
