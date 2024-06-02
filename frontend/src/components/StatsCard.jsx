43273128fd2cf0f70ad8165f7e44e447de67446c
madhavmadupu <134821170+madhavmadupu@users.noreply.github.com>
2024-06-02 16:13:51 +0530

    sidebar

--- frontend/src/components/StatsCard.jsx
+++ frontend/src/components/StatsCard.jsx
@@ -1,11 +1,52 @@
-import React from 'react'
+// https://mui.com/x/react-charts/pie/
+import * as React from 'react';
+import { PieChart } from '@mui/x-charts/PieChart';
+import { LineChart } from '@mui/x-charts/LineChart';
 
 const StatsCard = () => {
-  return (
-    <div className='h-full w-full flex flex-col justify-center items-center border-t border-black m-6'>
-        StatsCard
-    </div>
-  )
+    return (
+        <div>
+            <PieChart
+                series={[
+                    {
+                        data: [
+                            { id: 0, value: 10, label: 'series A' },
+                            { id: 1, value: 15, label: 'series B' },
+                            { id: 2, value: 20, label: 'series C' },
+                        ],
+                        innerRadius: 30,
+                        outerRadius: 100,
+                        paddingAngle: 5,
+                        cornerRadius: 5,
+                        startAngle: -90,
+                        endAngle: 180,
+                        cx: 150,
+                        cy: 150,
+                    },
+                ]}
+                width={400}
+                height={200}
+            />
+
+            <LineChart
+                xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+                series={[
+                    {
+                        data: [2, 5.5, 2, 8.5, 1.5, 5],
+                    },
+                ]}
+                width={500}
+                height={300}
+            />
+        </div>
+    );
 }
 
+
+
 export default StatsCard
+
+
+
+
+
