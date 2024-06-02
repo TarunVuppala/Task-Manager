dc3fdd73fb07c320b33af88289ce7775d272d079
madhavmadupu <134821170+madhavmadupu@users.noreply.github.com>
2024-06-02 17:22:03 +0530

    ToDo page

--- frontend/src/TaskBoard/TaskCard.jsx
+++ frontend/src/TaskBoard/TaskCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const TaskCard = () => {
   return (
-    <div>
+    <div className='border rounded-[20px] w-full h-fit flex flex-row items-center'>
         Task 1
     </div>
   )
--- frontend/src/TaskBoard/TasksPage.jsx
+++ frontend/src/TaskBoard/TasksPage.jsx
@@ -4,10 +4,42 @@ import TaskCard from './TaskCard';
 
 const TasksPage = ({ formOpen }) => {
     return (
-        <div className='flex flex-row'>
-            <div className='flex flex-col'>
-                <h1 className='font-black text-5xl'>ToDo</h1>
-                <TaskCard></TaskCard>
+        <div className='flex flex-row w-full'>
+            <div className='flex flex-row w-full'>
+                <div className='flex flex-col w-full'>
+                    <div className='flex flex-row items-center justify-between w-full'>
+
+                        <h1 className='font-black text-5xl m-8 px-50 w-full'>
+                            ToDo
+                        </h1>
+                        <select
+                            id="category"
+                            name="category"
+                            className='border rounded-[20px]  text-[#1E1E1E] py-1 flex  bg-transparent  font-black'
+                        >
+                            <option value="select tag">
+                                <div>
+
+                                </div>
+                            </option>
+                            <option value="personal">Personal</option>
+                            <option value="work">Work</option>
+                            <option value="home">Home</option>
+                        </select>
+                    </div>
+
+                    <div>
+                        <div className="flex flex-col gap-5">
+                            {Array.from({ length: 6 }).map((_, index) => (
+                                <div key={index} className="">
+                                    
+                                    <TaskCard></TaskCard>
+
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
             </div>
 
             {formOpen &&
