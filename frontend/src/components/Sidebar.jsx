ad084b7fa72bc459a23304d45f9e212b17ef1cf7
madhavmadupu <134821170+madhavmadupu@users.noreply.github.com>
2024-06-02 20:10:00 +0530

    Update Sidebar.jsx

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -5,7 +5,7 @@ import StatsCard from "./StatsCard";
 
 function Sidebar({ onLogout,user }) {
     return (
-        <div className='h-screen w-[400px] border-r flex flex-col justify-between items-center p-6'>
+        <div className='h-screen w-fit border-r flex flex-col justify-between items-center p-6 overflow-y-scroll'>
             <CalendarCard></CalendarCard>
             <StatsCard></StatsCard>
             <ProfileCard onLogout={onLogout} user={user}></ProfileCard>
