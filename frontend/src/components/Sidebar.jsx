510415c7d0d9cf50079084423d90765aba54394a
Manchikanti Varun <manchikantivarun05@gmail.com>
2024-06-03 21:21:27 +0530

    Calendar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import ProfileCard from "./ProfileCard";
 import CalendarCard from "./CalendarCard";
 import StatsCard from "./StatsCard";
+import Calendar from "./Calendar";
 
 
 function Sidebar({ onLogout,username }) {
     return (
         <div className='h-screen w-fit border-r flex flex-col justify-between items-center p-6 overflow-y-scroll overflow-x-hidden'>
-            <CalendarCard></CalendarCard>
+            <Calendar/>
             <StatsCard></StatsCard>
             <ProfileCard onLogout={onLogout} username={username}></ProfileCard>
         </div>
